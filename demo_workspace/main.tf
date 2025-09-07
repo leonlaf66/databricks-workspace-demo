ff487a6e3cbf68e906422b95152307df9dbf1464
Leon <leonlaf66@gmail.com>
2025-09-07 14:11:13 -0400

    test

--- demo_workspace/main.tf
+++ demo_workspace/main.tf
@@ -8,6 +8,9 @@ locals {
 }
 
 module "demo_workspace" {
+  providers = {
+    databricks = databricks.accounts
+  }
   source = "git::https://github.com/leonlaf66/databricks_workspace_tf_module.git?ref=init"
   databricks_account_id = local.databricks_account_id
   workspace_name        = "demo-workspace"
@@ -15,5 +18,5 @@ module "demo_workspace" {
   common_tags           = local.common_tags
   create_metastore      = true
   metastore_owner       = "Admins"
-  subnet_cidrs          = ["172.31.80.0/24", "172.31.81.0/24", "172.31.82.0/24"]
+  subnet_cidrs          = ["172.31.96.0/24", "172.31.97.0/24", "172.31.98.0/24"]
 }
